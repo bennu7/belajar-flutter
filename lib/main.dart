bf8ee26629bc84284e4e9553ba9b64415d6ca61e
bennu7 <laluibnuhidayatullah@gmail.com>
2020-10-15 02:46:34 +0700

    fluter 43 & flutter 42 problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,142 +1,142 @@
-// Belajar State Management Provider
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'belajar_provider/pages/HomePage.dart';
-import 'belajar_provider/provider/BlocUser.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Buatlah return nya ke ChangeNotifier agar data di keseluruhan page
-    // Selalu diupdate
-    // Yang mana valuenya kita arahkan ke pada file BlocUser
-    return ChangeNotifierProvider<BlocUser>.value(
-      value: BlocUser(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: HomePage(),
-        debugShowCheckedModeBanner: false,
-      ),
-    );
-  }
-}
-
-// // Fllutter 43 Provide State MAnegement (Multi provider)
-// // // tambahkan provider pada pubscey caml
+// // Belajar State Management Provider
 // import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
-// import '43_1_cart.dart';
-// import '43_2_money.dart';
+// import 'belajar_provider/pages/HomePage.dart';
+// import 'belajar_provider/provider/BlocUser.dart';
 
 // void main() => runApp(MyApp());
 
 // class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: MultiProvider(
-//         providers: [
-//           ChangeNotifierProvider<Money>(
-//             builder: (context) => Money(),
-//           ),
-//           ChangeNotifierProvider<Cart>(
-//             builder: (context) => Cart(),
-//           )
-//         ],
-//         child: Scaffold(
-//           floatingActionButton: Consumer<Money>(
-//             builder: (context, money, _) => Consumer<Cart>(
-//               builder: (context, cart, _) => FloatingActionButton(
-//                 onPressed: () {
-//                   if (money.balance >= 500) {
-//                     cart.quantity += 1;
-//                     money.balance -= 500;
-//                   }
-//                 },
-//                 child: Icon(Icons.add_shopping_cart),
-//                 backgroundColor: Colors.purple,
-//               ),
-//             ),
-//           ),
-//           appBar: AppBar(
-//             title: Text("Multi Provider"),
-//           ),
-//           body: Center(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: <Widget>[
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.center,
-//                   children: <Widget>[
-//                     Text("Balance"),
-//                     Container(
-//                       child: Align(
-//                         alignment: Alignment.centerRight,
-//                         child: Consumer<Money>(
-//                           builder: (context, money, _) => Text(
-//                             money.balance.toString(),
-//                             style: TextStyle(
-//                                 color: Colors.purple,
-//                                 fontWeight: FontWeight.w700),
-//                           ),
-//                         ),
-//                       ),
-//                       height: 30,
-//                       width: 150,
-//                       margin: EdgeInsets.all(5),
-//                       padding: EdgeInsets.all(5),
-//                       decoration: BoxDecoration(
-//                         borderRadius: BorderRadius.circular(5),
-//                         color: Colors.purple[100],
-//                         border: Border.all(color: Colors.purple, width: 2),
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//                 Container(
-//                   child: Align(
-//                     alignment: Alignment.centerRight,
-//                     child: Consumer<Cart>(
-//                       builder: (context, cart, _) => Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                         children: [
-//                           Text(
-//                             "Apple (500) x " + cart.quantity.toString(),
-//                             style: TextStyle(
-//                                 color: Colors.black,
-//                                 fontWeight: FontWeight.w500),
-//                           ),
-//                           Text(
-//                             (500 * cart.quantity).toString(),
-//                             style: TextStyle(
-//                                 color: Colors.black,
-//                                 fontWeight: FontWeight.w500),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                   height: 30,
-//                   margin: EdgeInsets.all(5),
-//                   padding: EdgeInsets.all(5),
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.circular(5),
-//                     border: Border.all(color: Colors.black, width: 2),
-//                   ),
-//                 )
-//               ],
-//             ),
-//           ),
+//     // Buatlah return nya ke ChangeNotifier agar data di keseluruhan page
+//     // Selalu diupdate
+//     // Yang mana valuenya kita arahkan ke pada file BlocUser
+//     return ChangeNotifierProvider<BlocUser>.value(
+//       value: BlocUser(),
+//       child: MaterialApp(
+//         title: 'Flutter Demo',
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
 //         ),
+//         home: HomePage(),
+//         debugShowCheckedModeBanner: false,
 //       ),
 //     );
 //   }
 // }
+
+// Fllutter 43 Provide State MAnegement (Multi provider)
+// // tambahkan provider pada pubscey caml
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '43_1_cart.dart';
+import '43_2_money.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Money>(
+            builder: (context) => Money(),
+          ),
+          ChangeNotifierProvider<Cart>(
+            builder: (context) => Cart(),
+          )
+        ],
+        child: Scaffold(
+          floatingActionButton: Consumer<Money>(
+            builder: (context, money, _) => Consumer<Cart>(
+              builder: (context, cart, _) => FloatingActionButton(
+                onPressed: () {
+                  if (money.balance >= 500) {
+                    cart.quantity += 1;
+                    money.balance -= 500;
+                  }
+                },
+                child: Icon(Icons.add_shopping_cart),
+                backgroundColor: Colors.purple,
+              ),
+            ),
+          ),
+          appBar: AppBar(
+            title: Text("Multi Provider"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Balance"),
+                    Container(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Consumer<Money>(
+                          builder: (context, money, _) => Text(
+                            money.balance.toString(),
+                            style: TextStyle(
+                                color: Colors.purple,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                      height: 30,
+                      width: 150,
+                      margin: EdgeInsets.all(5),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.purple[100],
+                        border: Border.all(color: Colors.purple, width: 2),
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Consumer<Cart>(
+                      builder: (context, cart, _) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Apple (500) x " + cart.quantity.toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            (500 * cart.quantity).toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  height: 30,
+                  margin: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.black, width: 2),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 // // Flutter 42 Provider State Managemenet
 // // tambahkan provider pada pubscey caml
